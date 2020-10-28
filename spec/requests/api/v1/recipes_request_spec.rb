a2f866a5dc30373bf38d931f4d5f77a2ce101162
Sabrina Robinson <sabrinarobinson@Sabrinas-MacBook-Pro.local>
2020-10-28 11:02:00 -0600

    ATP - Index action tests and model tests all passing

--- spec/requests/api/v1/recipes_request_spec.rb
+++ spec/requests/api/v1/recipes_request_spec.rb
@@ -8,6 +8,26 @@ describe "Recipes API" do
 
         expect(response).to  be_successful
 
-        recipes = JSON.parse(response.body)
+        recipes = JSON.parse(response.body, symbolize_names: true)
+
+        expect(recipes.count).to eq(3)
+        
+        recipes.each do |recipe|
+            
+            expect(recipe).to have_key(:id)
+            expect(recipe[:id]).to be_an(Integer)
+
+            expect(recipe).to have_key(:title)
+            expect(recipe[:title]).to be_an(String)
+
+            expect(recipe).to have_key(:image)
+            expect(recipe[:image]).to be_an(String)
+
+            expect(recipe).to have_key(:description)
+            expect(recipe[:description]).to be_an(String)
+
+            expect(recipe).to have_key(:chef_name)
+            expect(recipe[:chef_name]).to be_an(String)
+        end
     end
 end
